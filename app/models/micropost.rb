d5b18696d5b0836c0db34bad0aac43a5340db4ef
Technolo <technolo@mail.ru>
2012-11-04 11:14:08 +0200

    First models

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+﻿class Micropost < ActiveRecord::Base
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 140 }
+end
---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+﻿class User < ActiveRecord::Base
+  has_many :microposts
+end
